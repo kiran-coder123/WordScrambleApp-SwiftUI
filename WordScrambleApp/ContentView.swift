7ddb18f15ff77ff81a249b0951a70041c1e608f9
kiran sonne <kiransonne04@gmail.com>
2022-11-22 14:57:55 +0530

    Initial Commit

---
+++ WordScrambleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScrambleApp
+//
+//  Created by Kiran Sonne on 22/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordScrambleApp/WordScrambleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleAppApp.swift
+//  WordScrambleApp
+//
+//  Created by Kiran Sonne on 22/11/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
